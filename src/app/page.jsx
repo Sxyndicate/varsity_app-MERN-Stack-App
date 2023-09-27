1010057a373f336d2fad3c9816d81719f5f7e880
Sxyndicate <99019754+Sxyndicate@users.noreply.github.com>
2023-09-27 16:35:30 +0000

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import CardList from "@/components/cardList/CardList";
-import styles from "./blogPage.module.css";
+import styles from "./blog/blogPage.module.css"; 
 import Menu from "@/components/Menu/Menu";
 
 const BlogPage = ({ searchParams }) => {
